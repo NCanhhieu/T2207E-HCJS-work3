95421f11f5832b4027bba3c0c667c2a154bbc990
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2022-09-06 12:09:10 +0700

    nop bt

---
+++ JS/songuyento.js
@@ -0,0 +1,28 @@
+var x = 2;
+var t = 0;
+var flag = true;
+function prime() {
+
+    var a = document.getElementById("prime");
+    var b = document.getElementById("timer");
+
+        for (var i = 2; i < x - 1; i++) {  //kiemtrax
+            if (x % i == 0) {
+                flag = false;
+            }
+        }
+        if (flag) {                        //x thoa man thi in
+            t++;
+            a.innerText += " " + x;
+        }
+        else {
+            flag = true;
+        }
+    x++;
+        b.innerText = t;
+
+    if(t>=20){
+        clearInterval(si); // xoa set interval
+    }
+}
+var si = setInterval(prime,3000);
